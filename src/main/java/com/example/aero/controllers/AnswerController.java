649f3ee7d9f83f52f4bd65aa70cd61dd3a6f7cd9
yashith <yashith98@gmail.com>
2023-11-19 07:26:03 +0530

    Change base Urls

--- src/main/java/com/example/aero/controllers/AnswerController.java
+++ src/main/java/com/example/aero/controllers/AnswerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/answer")
+@RequestMapping("api/v1/answers")
 public class AnswerController {
     @Autowired
     AnswerService answerService;
--- src/main/java/com/example/aero/controllers/QuestionController.java
+++ src/main/java/com/example/aero/controllers/QuestionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/question")
+@RequestMapping("api/v1/questions")
 public class QuestionController {
 
     @Autowired
